b65a15f5928a39f5b41cb3d6d3d5bbd49e009c6b
Felipe <felipeangelissilva@hotmail.com>
2017-11-29 14:07:45 -0500

    user's page

--- bolsista/urls.py
+++ bolsista/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from bolsista import views
 
 urlpatterns = [
-    url(r'^$', views.main, name='main')
+    url(r'^$', views.main, name='main'),
+
+    url(r'^meuperfil$', views.meuperfil, name='meuperfil'),
 ]
--- bolsista/views.py
+++ bolsista/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def main(request):
     return render(request, 'bolsista/teste.html', {})
+
+
+def meuperfil(request):
+	return render(request, 'bolsista/meuperfil.html', {'content_title':'Atualize seus dados'})
