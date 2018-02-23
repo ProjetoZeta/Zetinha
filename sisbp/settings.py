e660ddec00de58a80798209e504a057f398db47f
Bruno Justino Garcia Praciano <bruno.praciano@latitude.unb.br>
2018-02-23 13:54:07 -0300

    Update settings.py

--- sisbp/settings.py
+++ sisbp/settings.py
@@ -83,11 +83,8 @@ WSGI_APPLICATION = 'sisbp.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'default.PRODUCTION': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sbpl',
         'USER': 'postgres',
