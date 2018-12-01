b67e80ac0529c49abd1c1a85edfb59861432e916
gresendesa <gresendesa@gmail.com>
2018-11-30 23:46:00 -0200

    Carregamento de dado pelo cep adicionado ao template base

--- cadastro/static/cadastro/bolsista-scripts.js
+++ cadastro/static/cadastro/bolsista-scripts.js
@@ -1,14 +1,6 @@
 
         $(document).ready(function () {
-
-            $("#id_cpf").mask('000.000.000-00', {reverse: true});
-            $("#id_inicio_vigencia").mask('00/00/0000', {reverse: true});
-            $('#id_telefone').mask('(00) 0000-0000');
-            $('#id_celular').mask('(00) 0 0000-0000');
-            $('#id_cep').mask('00.000-000');
-            $('#id_valor_mensal')
-            
-     
+                 
             $("#id_periodo_total").on("click", function (event) {
                 event.preventDefault();
                 var inicio = $('#id_inicio_vigencia').val()
@@ -26,59 +18,3 @@
 
 
         });
-
-
-    $(document).ready(function() {
-
-         function limpa_formulário_cep() {
-             // Limpa valores do formulário de cep.
-             $("#id_endereco").val("");
-             $("#id_cidade").val("");
-         }
-
-         //Quando o campo cep perde o foco.
-         $("#id_cep").blur(function() {
-
-             //Nova variável "cep" somente com dígitos.
-             var cep = $(this).val().replace(/\D/g, '');
-
-             //Verifica se campo cep possui valor informado.
-             if (cep != "") {
-
-                 //Expressão regular para validar o CEP.
-                 var validacep = /^[0-9]{8}$/;
-
-                 //Valida o formato do CEP.
-                 if(validacep.test(cep)) {
-
-                     //Preenche os campos com "..." enquanto consulta webservice.
-                     $("#id_endereco").val("...");
-                     $("#id_cidade").val("...");
-
-                     //Consulta o webservice viacep.com.br/
-                     $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
-
-                         if (!("erro" in dados)) {
-                             //Atualiza os campos com os valores da consulta.
-                             $("#id_endereco").val(dados.logradouro);
-                             $("#id_cidade").val(dados.localidade);
-                         } //end if.
-                         else {
-                             //CEP pesquisado não foi encontrado.
-                             limpa_formulário_cep();
-                             alert("CEP não encontrado.");
-                         }
-                     });
-                 } //end if.
-                 else {
-                     //cep é inválido.
-                     limpa_formulário_cep();
-                     alert("Formato de CEP inválido.");
-                 }
-             } //end if.
-             else {
-                 //cep sem valor, limpa formulário.
-                 limpa_formulário_cep();
-             }
-         });
-     });
