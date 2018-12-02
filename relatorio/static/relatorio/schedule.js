b5863b1da0ab83559d4a0737f7d18ae16490b866
gresendesa <gresendesa@gmail.com>
2018-12-02 00:09:46 -0200

    Construção de schedulejs progressos

--- relatorio/static/relatorio/schedule.js
+++ relatorio/static/relatorio/schedule.js
@@ -1,12 +1,12 @@
 class Tag {
 
-	constructor(name, inner, attrs=[]){
+	constructor({name, inner, attrs={}}={}){
 		this.name = name
 		this.inner = inner 
 		this.attrs = attrs
 	}
 
-	stringfyAttrs() {
+	stringifyAttrs() {
 		var str_attrs = []
 		this.attrs.forEach(function(attr){
 			str_attrs.push(attr.content)
@@ -15,14 +15,14 @@ class Tag {
 	}
 
 	get content(){
-		return `<${this.name}${this.attrs.length ? ' '+this.stringfyAttrs() : ''}>${this.inner}</${this.name}>`
+		return `<${this.name}${this.attrs.length ? ' '+this.stringifyAttrs() : ''}>${this.inner}</${this.name}>`
 	}
 
 };
 
 Tag.Attribute = class {
 
-	constructor(name, values=[]){
+	constructor({name, values}={}){
 		this.name = name
 		this.values = values
 	}
@@ -32,44 +32,94 @@ Tag.Attribute = class {
 	}
 }
 
-class Task {
 
-	constructor(title, initial_date, end_date) {
-		this.initial_date = initial_date; 
-		this.end_date = end_date;
+class Schedule {
+
+	constructor({tasks, config={'resolution': 5}}={}){
+		this.config = config
+		this.tasks = tasks
+		this.timeBorderMax = this.getTimeBorders().max
+		this.timeBorderMin = this.getTimeBorders().min
+	}
+
+	getTimeBorders() {
+		var min = Number.MAX_SAFE_INTEGER
+		var max = 0
+		this.tasks.forEach(function(task){
+			if(task.initial_date.getTime() < min)
+				min = task.initial_date.getTime()
+			if(task.end_date.getTime() > max)
+				max = task.end_date.getTime()
+		})
+		return {
+			'min': min,
+			'max': max,
+		}
+	}
+
+ 	get HTMLTable() {
+		return Schedule.HTMLTable({schedule: this, config: this.config})
+	}
+
+}
+
+Schedule.Task = class {
+
+	constructor({title, initial_date, end_date}={}) {
+		this.title = title
+		this.initial_date = new Date(initial_date); 
+		this.end_date = new Date(end_date);
 	}
 }
+
+Schedule.HTMLTable = class {
+
+	constructor({schedule, config}={}){
+		this.schedule = schedule
+		this.config
+	}
+
+	htmlTimeCells(task){
+		
+	}
+
+	get html(){
+		
+	}
+
+}
+
 
 data = [
 
 	{
 		'title': 'task0', 
-		'initial_date': '2018-01-01', 
-		'end_date': '2018-02-01'
+		'initial_date': '2011-10-10', 
+		'end_date': '2011-10-11'
 	},
 
 	{
 		'title': 'task1', 
-		'initial_date': '2018-01-01', 
-		'end_date': '2018-02-01'
+		'initial_date': '2011-10-10', 
+		'end_date': '2018-10-11'
 	},
 
 	{
 		'title': 'task2', 
-		'initial_date': '2018-01-01', 
-		'end_date': '2018-02-01'
+		'initial_date': '2011-10-10', 
+		'end_date': '2011-10-11'
 	},
 
 	{
 		'title': 'task3', 
-		'initial_date': '2018-01-01', 
-		'end_date': '2018-02-01'
+		'initial_date': '2011-10-10', 
+		'end_date': '2011-10-11'
 	},
 
 	{
 		'title': 'task4', 
-		'initial_date': '2018-01-01', 
-		'end_date': '2018-02-01'
+		'initial_date': '2011-10-10', 
+		'end_date': '2011-10-11'
 	},
 
 ]
@@ -77,15 +127,20 @@ data = [
 var tasks = []
 
 data.forEach(function(elem){
-	tasks.push(new Task(elem.title, elem.initial_date, elem.end_date))
+	tasks.push(new Schedule.Task({title: elem.title, initial_date: elem.initial_date, end_date: elem.end_date}))
 })
 
-c = new Tag.Attribute('class', ['hello', 'motherfucker'])
-d = new Tag.Attribute('for', ['mean', 'ok'])
+schedule = new Schedule({tasks: tasks})
 
-tag = new Tag('div', 'hello', attrs=[c, d])
+c = new Tag.Attribute({name: 'class', values: ['hello', 'motherfucker']})
+d = new Tag.Attribute({name: 'for', values: ['mean', 'ok']})
 
-console.log(tag.content)
+tag = new Tag({name: 'div', inner: 'hello', attrs: [c, d]})
+
+console.log(schedule)
+
+
+
 
 
 
