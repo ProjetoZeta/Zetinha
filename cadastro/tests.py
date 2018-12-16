d4cebdabdbd64f08efb810ec457440a6952dd554
gresendesa <gresendesa@gmail.com>
2018-12-16 03:55:58 -0200

    arquivos desnecessários removidos

--- cadastro/tests.py
+++ cadastro/tests.py
@@ -1,15 +1,1 @@
 from django.test import TestCase
-from .factories import EmpregoFactory
-
-# Create your tests here.
-
-class MockData(TestCase):
-
-	def setUp(self):
-		pass
-
-	def create(self):
-		a = EmpregoFactory.build()
-		a.save()
-			
-		
