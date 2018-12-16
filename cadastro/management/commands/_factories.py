5edafe15b521ecd2ff8250a42476a1c5b869c526
gresendesa <gresendesa@gmail.com>
2018-12-16 04:24:29 -0200

    Factory de Responsável criado

--- cadastro/management/commands/_factories.py
+++ cadastro/management/commands/_factories.py
@@ -1,5 +1,5 @@
 import factory
-from cadastro.models import Emprego
+from cadastro.models import Emprego, Responsavel
 
 faker = factory.faker.Faker._get_faker(locale='pt_BR')
 
@@ -8,3 +8,20 @@ class EmpregoFactory(factory.django.DjangoModelFactory):
 		model = Emprego
 	nome = factory.lazy_attribute(lambda x: faker.job()) 
 	tipo = factory.Iterator([Emprego.FUNCAO, Emprego.CARGO])
+
+class ResponsavelFactory(factory.django.DjangoModelFactory):
+	class Meta:
+		model = Responsavel
+
+	nome = factory.lazy_attribute(lambda x: faker.name_female())
+	email = factory.lazy_attribute(lambda x: faker.safe_email())
+	cpf = factory.lazy_attribute(lambda x: faker.cpf())
+	data_nascimento = factory.lazy_attribute(lambda x: faker.date_of_birth())
+	rg = factory.lazy_attribute(lambda x: faker.ssn())
+	orgao_expedidor = factory.lazy_attribute(lambda x: faker.currency_code())
+	telefone = '(00) 0000-0000'
+	celular = '(00) 9 0000-0000'
+	matricula = factory.lazy_attribute(lambda x: faker.ssn())
+	ic_ativo = True
+
+	
--- cadastro/management/commands/populatedb.py
+++ cadastro/management/commands/populatedb.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 import factory
-from ._factories import EmpregoFactory
+from ._factories import EmpregoFactory, ResponsavelFactory
 
 # see https://docs.djangoproject.com/en/2.1/howto/custom-management-commands/#writing-custom-django-admin-commands
 
@@ -8,6 +8,7 @@ class Command(BaseCommand):
     help = 'Populates the bd with mock data'
 
     def handle(self, *args, **options):
-        EmpregoFactory.create_batch(size=2)
+        EmpregoFactory.create_batch(size=10)
+        ResponsavelFactory.create_batch(size=10)
         
         
